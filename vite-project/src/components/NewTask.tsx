a4259371d133364d259647222618eced7596561b
Ricardo Souza <rl03souza@gmail.com>
2023-05-22 19:21:33 -0300

    Refactor: new task component with final form lib

--- vite-project/src/components/NewTask.tsx
+++ vite-project/src/components/NewTask.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
 import Task from "../Interfaces/Task"
 import UserStore from "../zustand/UserStore/UserStore"
-import Input from "./Input"
 import Button from "./Button"
+import { Form, Field } from "react-final-form";
 
 export default function NewTask() {
     const addTask = UserStore((state) => state.addTask)
@@ -10,7 +10,8 @@ export default function NewTask() {
     const [description, setDescription] = useState<string>('')
 
 
-    const createTask = () => {
+    const createTask = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        event.preventDefault()
         const newTask: Task = {
             name: name as string,
             description: description as string,
@@ -20,9 +21,76 @@ export default function NewTask() {
         setName(''), setDescription('')
     }
 
+    const validateForm = () => {
+        const errors = {name: '', description: ''}
+        if (!name) {
+            errors.name = 'Necessário'
+        }
+        if (name.length < 1) {
+            errors.name = 'Muito curto'
+        }
+        if (!description) {
+            errors.description = 'Necessário'
+        }
+        if (description && description.length < 4) {
+            errors.description = 'Muito curto'
+        }
+        return errors
+    }
+
     return (
         <section>
-            <Input 
+            <Form 
+                onSubmit={createTask}
+                validate={validateForm}
+                render={({handleSubmit, submitting}) => (
+                    <form onSubmit={handleSubmit}>
+                        <Field name="name">
+                            {({ input, meta }) => (
+                                <div>
+                                    <label>Nome: </label>
+                                    <input 
+                                        type="text" 
+                                        {...input}
+                                        placeholder="Digite um nome"
+                                        name="name"
+                                        onChange={(event) => setName(event.target.value)}
+                                        value={name}
+                                    />
+                                    { meta.touched && meta.error && <span>{meta.error}</span> }
+                                </div>
+                            )}
+                        </Field>
+                        <Field name="description">
+                            {({ input, meta }) => (
+                                <div>
+                                    <label>Descrição: </label>
+                                    <input 
+                                        type="text" 
+                                        {...input}
+                                        placeholder="Descreva-a aqui"
+                                        name="description"
+                                        onChange={(event) => setDescription(event.target.value)}
+                                        value={description}
+                                    />
+                                    { meta.touched && meta.error && <span>{meta.error}</span> }
+                                </div>
+                            )}
+                        </Field>
+                        <Button 
+                            disabled={submitting}
+                            name="Adicionar"
+                            onClick={(event) => createTask(event)}
+                            type="submit"
+                        />
+                    </form>
+                )}
+            />
+        </section>
+    )
+}
+
+{/* <Input 
                 type="text"
                 name="Nome"
                 onChange={(event) => setName(event.target.value)}
@@ -41,7 +109,4 @@ export default function NewTask() {
                 disabled={false}
                 name="Criar"
                 type="button"
-            />
-        </section>
-    )
-}
+            /> */}
