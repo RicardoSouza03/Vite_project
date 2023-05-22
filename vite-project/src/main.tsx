f1ff47e13a4d587831e8fe82b28022525bcb42f0
Ricardo Souza <rl03souza@gmail.com>
2023-05-22 17:02:03 -0300

    Feat: create router with routes

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider, createBrowserRouter} from 'react-router-dom'
 import './index.css'
+import Login from './Pages/Login.tsx'
+import List from './Pages/List.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/list',
+    element: <List />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
