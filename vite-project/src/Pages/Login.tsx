e55197c8dd7ddd7ef2555f4ba49da88c88ec6d22
Ricardo Souza <rl03souza@gmail.com>
2023-05-23 14:43:29 -0300

    Style: create css for login page

--- vite-project/src/Pages/Login.tsx
+++ vite-project/src/Pages/Login.tsx
@@ -3,6 +3,7 @@ import UserStore from "../zustand/UserStore/UserStore";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 import { Form, Field } from "react-final-form";
+import "../Css/Login.css"
 
 export default function Login() {
     const userStore = UserStore()
@@ -29,49 +30,53 @@ export default function Login() {
     }
 
     return (
-        <Form 
-            onSubmit={submitHandler}
-            validate={validateForm}
-            render={({handleSubmit, submitting}) => (
-                <form onSubmit={handleSubmit}>
-                    <Field name="email" >
-                        {({ input, meta }) => (
-                            <div>
-                                <label>Email: </label>
-                                <input
-                                    {...input}
-                                    onChange={(event) => setEmail(event.target.value)} 
-                                    placeholder="Email"
-                                    type="text"
-                                    value={email}
-                                />
-                                { meta.touched && meta.error && <span>{meta.error}</span> }
-                            </div>
-                        )}
-                    </Field>
-                    <Field name="password">
-                        {({ input, meta }) => (
-                            <div>
-                                <label>Senha: </label>
-                                <input
-                                    type="text"
-                                    {...input}
-                                    placeholder="Senha"
-                                    onChange={(event) => setPassword(event.target.value)}
-                                    value={password}
-                                />
-                                { meta.touched && meta.error && <span>{meta.error}</span> }
-                            </div>
-                        )}
-                    </Field>
-                    <Button 
-                        disabled={submitting}
-                        name="Entrar"
-                        onClick={(event) => submitHandler(event)}
-                        type="submit"
-                    />
-                </form>
-            )}
-        />
+        <main className="login_main">
+            <h2>Login</h2>
+            <Form 
+                onSubmit={submitHandler}
+                validate={validateForm}
+                render={({handleSubmit, submitting}) => (
+                    <form onSubmit={handleSubmit}>
+                        <Field name="email">
+                            {({ input, meta }) => (
+                                <fieldset>
+                                    <label htmlFor="email">E-mail: </label>
+                                    <input
+                                        {...input}
+                                        onChange={(event) => setEmail(event.target.value)} 
+                                        placeholder="Email"
+                                        type="text"
+                                        value={email}
+                                        id="email"
+                                        />
+                                    { meta.touched && meta.error && <span>{meta.error}</span> }
+                                </fieldset>
+                            )}
+                        </Field>
+                        <Field name="password">
+                            {({ input, meta }) => (
+                                <fieldset>
+                                    <label>Senha: </label>
+                                    <input
+                                        type="password"
+                                        {...input}
+                                        placeholder="Senha"
+                                        onChange={(event) => setPassword(event.target.value)}
+                                        value={password}
+                                        />
+                                    { meta.touched && meta.error && <span>{meta.error}</span> }
+                                </fieldset>
+                            )}
+                        </Field>
+                        <Button 
+                            disabled={submitting}
+                            name="Entrar"
+                            onClick={(event) => submitHandler(event)}
+                            type="submit"
+                            />
+                    </form>
+                )}
+                />
+        </main>
     )
 }
